aa590de95155ee563d44165727e7339e2509d629
Andrei Rohatin <andrei.rohatin@codemelt.com>
2025-02-03 14:15:17 +0200

    fix rewrite

--- src/api/[[route]].ts
+++
@@ -1,8 +0,0 @@
-import { handle } from '@hono/node-server/vercel'
-import app from './[[...route]]'
-
-export const config = {
-  runtime: 'edge',
-}
-
-export default handle(app)
---
+++ src/api/index.ts
@@ -0,0 +1,15 @@
+import { handle } from 'hono/vercel'
+import app from './[[...route]]'
+
+export const config = {
+  runtime: 'edge',
+}
+
+
+const handler = handle(app);
+
+export const GET = handler;
+export const POST = handler;
+export const PATCH = handler;
+export const PUT = handler;
+export const OPTIONS = handler;
