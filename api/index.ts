1cfaa9e9d6c1dc85dcfd26ab3765d52340425882
Andrei Rohatin <andrei.rohatin@codemelt.com>
2025-02-03 14:34:51 +0200

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,14 @@
-import { handle } from '@hono/node-server/vercel'
+import { handle } from 'hono/vercel'
 import app from '../src/api/[[...route]]'
 
 export const config = {
   runtime: 'edge',
 }
 
-export default handle(app)
+const handler = handle(app);
+
+export const GET = handler;
+export const POST = handler;
+export const PATCH = handler;
+export const PUT = handler;
+export const OPTIONS = handler;
